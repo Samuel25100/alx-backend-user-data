3f7993a4a47186caf5ffc52e0513aac641da7ac7
Samuel25100 <samuelgselassie1@gmail.com>
2024-08-30 15:04:06 +0300

    fixing documentation

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,14 +2,14 @@
 """function: filter_datum"""
 import logging
 import re
+from typing import List
 
-logging.basicConfig(level=logging.INFO)
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
     """returns the log message obfuscated"""
-    out = []
     for field in fields:
         pattern = r"{}=.*?{}".format(field, separator)
-        message = re.sub(pattern, r"{}={}{}".format(field, redaction, separator),
-                      message)
+        message = re.sub(pattern, r"{}={}{}".format(field, redaction,
+                                                    separator), message)
     return message
