332fdca9deda3a682dddc512cf1bfe767ce2bf30
Samuel25100 <samuelgselassie1@gmail.com>
2024-08-31 19:06:17 +0300

    add files for task-1

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -13,3 +13,22 @@ def filter_datum(fields: List[str], redaction: str,
         message = re.sub(pattern, r"{}={}{}".format(field, redaction,
                                                     separator), message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        message = super(RedactingFormatter, self).format(record)
+        result = filter_datum(self.fields, self.REDACTION,
+                              message, self.SEPARATOR)
+        return (result)
