f0ee2ee024af03a81115b8fccd232d2d0f947451
Samuel25100 <samuelgselassie1@gmail.com>
2024-08-31 19:12:21 +0300

    fixing file filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -24,10 +24,12 @@ class RedactingFormatter(logging.Formatter):
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
+        """constructor of RedactingFormatter"""
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
+        """return formated and obfuscated message"""
         message = super(RedactingFormatter, self).format(record)
         result = filter_datum(self.fields, self.REDACTION,
                               message, self.SEPARATOR)
