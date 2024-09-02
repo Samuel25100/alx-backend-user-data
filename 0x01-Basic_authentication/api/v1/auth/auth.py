69f47675377c140be654cac128c33b94040adcf0
Samuel25100 <samuelgselassie1@gmail.com>
2024-09-02 16:10:30 +0300

    modify the file auth/auth.py for task-4

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -9,7 +9,14 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """returns False - path and excluded_paths"""
-        return False
+        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        if path in excluded_paths:
+            return False
+        if path == "/api/v1/status" and "/api/v1/status/" in excluded_paths:
+            return False
+        else:
+            return True
 
     def authorization_header(self, request=None) -> str:
         return None
