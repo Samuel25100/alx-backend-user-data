43e775647ec79a00666ac52195b95dc985143081
Samuel25100 <samuelgselassie1@gmail.com>
2024-09-03 12:37:46 +0300

    modify file auth/basic_auth.py for task_10

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 """basic auth"""
-from api.v1.auth.auth import Auth
 import base64
+from api.v1.auth.auth import Auth
+from models.user import User
+from typing import TypeVar
 
 
 class BasicAuth(Auth):
@@ -44,3 +46,19 @@ class BasicAuth(Auth):
             return (None, None)
         result = decoded_base64_authorization_header.split(':')
         return (result[0], result[1])
+
+    def user_object_from_credentials(self, user_email: str,
+                            user_pwd: str) -> TypeVar('User'):
+        """returns the User instance based on his email and password"""
+        if not user_email or not isinstance(user_email, str):
+            return None
+        if not user_pwd or not isinstance(user_pwd, str):
+            return None
+        user = User()
+        lsuser = user.search({'email': user_email})
+        if not lsuser:
+            return None
+        for i in lsuser:
+            if i.is_valid_password(user_pwd):
+                return i
+        return None
