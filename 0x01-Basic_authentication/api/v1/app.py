3a8d8160037f4431f083f7f231aa95978c04d7e2
Samuel25100 <samuelgselassie1@gmail.com>
2024-09-03 16:03:00 +0300

    making file executable

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -6,18 +6,20 @@ from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
-import os
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
-auth = None
+
 auth = getenv('AUTH_TYPE')
-if auth:
+if auth == "basic_auth":
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
+elif auth == "auth":
+    from api.v1.auth.auth import Auth
+    auth = Auth()
 
 
 @app.before_request
