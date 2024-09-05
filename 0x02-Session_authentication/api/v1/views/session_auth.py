5b1219b08c5b8241a1cd5482289ccef55ae3374a
Samuel25100 <samuelgselassie1@gmail.com>
2024-09-05 19:30:38 +0300

    fixing file api/v1/views/session_auth.py for task-7

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -13,12 +13,12 @@ def post_in_session():
     email = request.form.get('email')
     password = request.form.get('password')
     if not email:
-        return jsonify({ "error": "email missing" }), 400
+        return jsonify({"error": "email missing"}), 400
     if not password:
-        return jsonify({ "error": "password missing" }), 400
+        return jsonify({"error": "password missing"}), 400
     users = User.search({'email': email})
     if not users:
-        return jsonify({ "error": "no user found for this email" }), 404
+        return jsonify({"error": "no user found for this email"}), 404
     for user in users:
         if user.is_valid_password(password):
             from api.v1.app import auth
@@ -27,4 +27,4 @@ def post_in_session():
             out = jsonify(user.to_json())
             out.set_cookie(session_nm, session_id)
             return out
-    return jsonify({ "error": "wrong password" }), 401
+    return jsonify({"error": "wrong password"}), 401
