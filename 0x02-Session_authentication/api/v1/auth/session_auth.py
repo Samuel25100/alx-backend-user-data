a8f0106b6a3af018aa44cf443b8b6a70dbdad244
Samuel25100 <samuelgselassie1@gmail.com>
2024-09-05 13:36:43 +0300

    add session id creator

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,7 +1,20 @@
 #!/usr/bin/env python3
 """Session authentication"""
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """Session authentication class"""
+    def __init__(self):
+        """constructor"""
+        self.user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """create session id for user"""
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        session_id = str(uuid.uuid4())
+        print(self.user_id_by_session_id)
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
