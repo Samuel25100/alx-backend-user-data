4f4fa4d2f38ccf97f35e08d2cdc1cc1c83f13d46
Samuel25100 <samuelgselassie1@gmail.com>
2024-09-06 16:10:27 +0300

    add file api/v1/auth/session_exp_auth.py

---
+++ 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""Session authentication expiration"""
+import datetime
+from api.v1.auth.session_auth import SessionAuth
+from os import getenv
+
+
+class SessionExpAuth(SessionAuth):
+    """handle expiration of session"""
+
+    def __init__(self):
+        """constructor for session att"""
+        self.session_duration: int = int(getenv('SESSION_DURATION', 0))
+
+    def create_session(self, user_id=None):
+        """overload create_session of SessionAuth"""
+        session_id = super().create_session(user_id)
+        if session_id is None:
+            return None
+        self.user_id_by_session_id[session_id] = user_id
+        self.user_id_by_session_id['created_at'] = datetime.datetime.now()
+        return session_id
+
+    def user_id_for_session_id(self, session_id=None):
+        """overload user_id_for_session_id of SessionAuth"""
+        if session_id is None:
+            return None
+        user_id = self.user_id_by_session_id.get(session_id)
+        if user_id is None:
+            return None
+        if self.session_duration <= 0:
+            return user_id
+        if 'created_at' not in self.user_id_by_session_id:
+            return None
+        dur = self.user_id_by_session_id['created_at']
+        dur += datetime.timedelta(seconds=self.session_duration)
+        print(f"###{dur}###")
+        now = datetime.datetime.now()
+        if dur < now:
+            return None
+        else:
+            return user_id
