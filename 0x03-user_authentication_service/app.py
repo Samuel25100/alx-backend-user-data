1d29b78d05b2611087cda87d6c17e50781a99617
Samuel25100 <samuelgselassie1@gmail.com>
2024-09-13 18:21:25 +0300

    fixing app logout route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -46,7 +46,7 @@ def logout():
     """handle logout based on session_id"""
     session_id = request.cookies.get('session_id')
     user = AUTH.get_user_from_session_id(session_id)
-    if user is None:
+    if user is None or not session_id:
         return abort(403)
     AUTH.destroy_session(user.id)
     return redirect('/')
