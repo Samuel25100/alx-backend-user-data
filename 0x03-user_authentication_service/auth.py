0f4270ed5ba834780f894a86a5363a778f6fc8d2
Samuel25100 <samuelgselassie1@gmail.com>
2024-09-13 16:40:26 +0300

    fixing auth type annotation

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -31,7 +31,7 @@ class Auth:
             nuser = self._db.add_user(email, hashed_password)
             return nuser
 
-    def valid_login(self, email: str, password: str):
+    def valid_login(self, email: str, password: str) -> bool:
         """validate give email and password is correct"""
         try:
             user = self._db.find_user_by(email=email)
