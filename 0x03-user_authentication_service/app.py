706a397bc47ef1fd09670ee14c9cb30693202062
Samuel25100 <samuelgselassie1@gmail.com>
2024-09-13 16:25:20 +0300

    fixing file app for task-7

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -20,7 +20,7 @@ def regs_user():
     try:
         user = auth.register_user(email, pwd)
         if user:
-            return jsonify({"email": email, "message": "user created"}), 400
+            return jsonify({"email": email, "message": "user created"}), 200
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
 
