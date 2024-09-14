39d6f4dfb0c77ff9c1930170b51883f16b36c23e
Samuel25100 <samuelgselassie1@gmail.com>
2024-09-14 10:39:22 +0300

    add route profile in app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -51,6 +51,15 @@ def logout():
     AUTH.destroy_session(user.id)
     return redirect('/')
 
+@app.route("/profile")
+def profile():
+    """handle route for profile of user"""
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if user is None or not session_id:
+        return abort(403)
+    return jsonify({"email": user.email}), 200
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
