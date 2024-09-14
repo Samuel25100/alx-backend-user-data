b1ea941582477cf7229a41a8d90f3b95bd539ece
Samuel25100 <samuelgselassie1@gmail.com>
2024-09-14 10:59:03 +0300

    add route handle in app for reset token

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -62,5 +62,18 @@ def profile():
     return jsonify({"email": user.email}), 200
 
 
+@app.route("/reset_password", methods=['POST'])
+def reset_password():
+    """handle the reset password token"""
+    email = request.cookies.get('email')
+    if not email:
+        return abort(403)
+    try:
+        usr_tk = AUTH.get_reset_password_token(email)
+        return jsonify({"email": email, "reset_token": usr_tk}), 200
+    except Exception:
+        return abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
