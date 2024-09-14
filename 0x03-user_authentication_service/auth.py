438f046fd159b3d00a9d68f6b3d8f4f10c561402
Samuel25100 <samuelgselassie1@gmail.com>
2024-09-14 11:34:54 +0300

    add update passord metho in auth

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -87,7 +87,7 @@ class Auth:
         try:
             user = self._db.find_user_by(reset_token=reset_token)
             hashed_password = _hash_password(password)
-            user.hashed_password = hashed_password
-            user.reset_token = None
+            self._db.update_user(user.id, hashed_password=hashed_password,
+                                 reset_token=None)
         except Exception:
             raise ValueError
